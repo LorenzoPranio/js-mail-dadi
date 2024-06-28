db6a4134a26077a430d81d445e78d5688be79784
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-28 17:58:25 +0200

    [UPD]Finished the excercise Dadi in script.js

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,7 +1,25 @@
 //Creare un ARREY vuoto
+//[0]giocatore [1]computer
 
 const numberz = []
 
 //Creare un CICLO FOR per riempire l'array con il math random
 
+for (let i = 1; i <= 2; i++){
+    numberz.push(Math.floor(Math.random() * 6 + 1))
+}
+
+console.log(numberz);
+
 //Creo un CICLO FOR per confrontare i numeri all'interno dell'ARRAY
+
+if (numberz[0] > numberz[1]){
+   console.log('Giocatore Win')
+}
+else if (numberz[0]< numberz[1]){
+    console.log('Computer Win')
+}
+else {
+    console.log('Pareggio')
+}
+
