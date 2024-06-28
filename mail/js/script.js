da2da687b8b2c04999734e51e24e0a2ee8fbf2f2
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-28 16:08:17 +0200

    [UPD]Added Array authorized_email in script.js

--- mail/js/script.js
+++ mail/js/script.js
@@ -10,6 +10,8 @@ let found_email = false;
 
 //Creare un ARREY con la lista delle mail
 
+const authorized_email = ["jacksparrow@gmail.com", "davyjones@gmail.com", "turnerwill@gmail.com",]
+
 
 //Ciclo l'ARRAY e confronto l'elemento che sto ciclando con la mail inserita nel prompt
 
